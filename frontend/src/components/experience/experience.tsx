ae09daa9aa08f434058681608c4b83f612a869d9
Augustin <augustinusmp@gmail.com>
2025-03-19 00:20:41 -0400

    added all pages

---
+++ frontend/src/components/experience/experience.tsx
@@ -0,0 +1,9 @@
+import Background from "../background/background";
+
+export default function Experience() {
+    return (
+        <Background>
+            <h1>Experience</h1>
+        </Background>
+    );
+}
---
+++ frontend/src/components/projects/projects.tsx
@@ -0,0 +1,9 @@
+import Background from "../background/background";
+
+export default function Projects() {
+    return (
+        <Background>
+            <h1>Projects</h1>
+        </Background>
+    );
+}
