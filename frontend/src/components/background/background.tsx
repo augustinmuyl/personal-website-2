673127eb66d468666ce81010aed7aa1a2c2d12ec
Augustin <augustinusmp@gmail.com>
2025-06-09 15:44:59 +0200

    animated background

--- frontend/src/components/background/background.tsx
+++ frontend/src/components/background/background.tsx
@@ -1,11 +1,19 @@
+"use client"
+
 import { ReactNode } from "react";
 import Navbar from "../navbar/navbar";
 import Footer from "../footer/footer";
+import { motion } from "motion/react"
 
 export default function Background({ children }: { children: ReactNode }) {
     return (
         <div className="h-screen p-6">
-            <div className="border h-full bg-radial from-[#0F172A] to-black relative">
+            <motion.div
+                className="border h-full"
+                style={{ background: "radial-gradient(circle, var(--tw-gradient-from), var(--tw-gradient-to))" }}
+                animate={{ "--tw-gradient-from": ["#0F172A", "#19213b", "#222b4a"] }}
+                transition={{ duration: 5, repeat: Infinity, repeatType: "reverse", ease: "easeInOut" }}
+            >
                 <div className="p-10 lg:mx-50 z-10 relative">
                     <Navbar />
                 </div>
@@ -15,7 +23,7 @@ export default function Background({ children }: { children: ReactNode }) {
                 <div className="absolute bottom-10 right-10">
                     <Footer />
                 </div>
-            </div>
+            </motion.div>
         </div>
     );
 }
