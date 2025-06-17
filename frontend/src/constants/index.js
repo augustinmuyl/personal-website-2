829045d95172674be3d5999469a8bbbf7c8fc74d
Augustin <augustinusmp@gmail.com>
2025-06-17 19:50:08 +0200

    added company logos

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,20 +1,22 @@
 import Image from "next/image";
 
-import Linkedin_Logo from "../../public/assets/linkedin-logo.svg";
+import CMA_CGM_Logo from "../../public/assets/cma_cgm_logo.png";
+import Iperuranium_Logo from "../../public/assets/iperuranium_logo.png";
+import Boston_Logo from "../../public/assets/bostonhacks_logo.png";
 
 const experiences = [
   {
     title: "Data Science Intern",
     company: "CMA CGM",
     date: "May 2025 - Aug 2025",
-    icon: <Image src={Linkedin_Logo} alt="Iperuranium" />,
+    icon: <Image src={CMA_CGM_Logo} alt="Iperuranium" />,
     description: ["Group Security & Intelligence team"],
   },
   {
     title: "Software Engineer",
     company: "Iperuranium",
     date: "Feb 2025 - Present",
-    icon: <Image src={Linkedin_Logo} alt="Iperuranium" />,
+    icon: <Image src={Iperuranium_Logo} alt="Iperuranium" />,
     description: [
       "Led frontend development with TypeScript, Next.js, and Tailwind CSS for core user-facing pages, aligning UI to Figma designs and ensuring a mobile-first responsive interface",
       "Implemented interactive UI animations with Motion (Framer Motion) across critical signup flows, enhancing UX engagement through clear animated feedback",
@@ -24,7 +26,7 @@ const experiences = [
     title: "Software Developer",
     company: "BostonHacks",
     date: "Feb 2025 - Present",
-    icon: <Image src={Linkedin_Logo} alt="BostonHacks" />,
+    icon: <Image src={Boston_Logo} alt="BostonHacks" />,
     description: [
       "Archived legacy hackathon web applications (2017-Present) as static HTML and collaborated with design to launch a unified, on-brand portal, enabling students and sponsors to explore them in a centralized, zero-maintenance archive",
     ],
