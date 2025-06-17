8ae73aea56e4823bd9bb6848bb9f82d3d5dc0e80
Augustin <augustinusmp@gmail.com>
2025-06-17 20:46:57 +0200

    added metadata and favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "Augustin's Personal Website",
+    description: "Hi, I'm a Math and Computer Science student at Boston University with a background in full-stack development, especially using Flask/FastAPI, React.js/Next.JS, Tailwind CSS, and SQL",
+};
 
 export default function RootLayout({
     children,
