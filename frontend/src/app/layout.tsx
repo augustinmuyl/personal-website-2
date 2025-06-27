35c58687a7f3e6be5588e1b04e7a7bb4d1f3b5c7
Augustin <augustinusmp@gmail.com>
2025-06-27 20:08:35 +0200

    changed font to inter

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,16 @@
 import { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google"
 
 export const metadata: Metadata = {
     title: "Augustin's Personal Website",
     description: "Hi, I'm a Math and Computer Science student at Boston University with a background in full-stack development, especially using Flask/FastAPI, React.js/Next.JS, Tailwind CSS, and SQL",
 };
+
+const inter = Inter({
+    subsets: ['latin'],
+    variable: '--font-inter',
+})
 
 export default function RootLayout({
     children,
@@ -12,8 +18,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className="bg-black text-white font-extralight">
+        <html lang="en" className={inter.variable}>
+            <body className="bg-black text-white font-extralight font-sans">
                 {children}
             </body>
         </html>
